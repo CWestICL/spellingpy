815740c1c8750049b559d098168a3109d05a011f
CWestICL <cwest@ic.ac.uk>
2022-10-06 22:59:37 +0100

    REST commit

--- app.py
+++ app.py
@@ -3,16 +3,64 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 
 import random
-import requests
-import aiohttp
+#import requests
+#import aiohttp
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
 db = SQLAlchemy(app)
 
+dictionary = open('dictionary.txt', 'r')
+word_list = dictionary.read().split("\n")
 
-challenges = ["ugsioandasoalrit","oagedeaoteaaohhd","ncrvoiizneotvrsn","evlogagjtordmest","kaouesexrsheagat","ftedfiotetuiabbf","yueimaedegladtir","vatisteejisrxvay","tesneeijfegpited","tzehumciwetdigoi","eangpiafitotolui","ehdoaopidaenarid","cetayefoerouexdi","efrosdoutogcfeas","cedsbneeenuaaqio","uppuegauutwdndvr"]
+
+challenges = [
+    "ehdoaopidaenarid",
+    "cetayefoerouexdi",
+    "efrosdoutogcfeas",
+    "cedsbneeenuaaqio",
+    "uppuegauutwdndvr",
+    "nsnuienwaqnadlee",
+    "oogatjnyrnatsiou",
+    "tineieqcssovsnie",
+    "qamesebahexosmgd",
+    "rltliaaremaeeuse",
+    "dicbopqeanueaflr",
+    "wqnceteeioxraeod",
+    "itduoiaetaarenbi",
+    "zsiieuiaerfvtiin",
+    "raydeeederieldmr",
+    "eaysueetjcreiamo",
+    "tuorsaujoeroniad",
+    "ivdltielbmisqsoa",
+    "rhhnndoeznmelooi",
+    "elesruawxegdlory",
+    "anzhtypnadadaiem",
+    "olteaaauranesqor",
+    "booreaiasugedavr",
+    "euctellooehrvlmi",
+    "iniprovdeoblleni",
+    "tenrddectoiaazdi",
+    "tnbanweciaielvfe",
+    "efrewaifteumtard",
+    "fetvlmaicoitdoir",
+    "onosnankredseauw",
+    "xoatwsodaiaitcrw",
+    "mgaealiedfeoxsoa",
+    "dwiiepkshathlvfe",
+    "vgdiollrtoouaaig",
+    "otbmaienniredegh",
+    "vgdiollrtoouaaig",
+    "sytneykoiasutett",
+    "stiphssokieemrot",
+    "oueeonwevaondher",
+    "agiulinorabliena",
+    "raaosineateleple",
+    "stetriufltteiipb",
+    "obraeossurdauetc",
+    "sooleoeaidttrern"
+]
 
 
 print("###-Challenge check-###")
@@ -22,24 +70,10 @@ for idx, chall in enumerate(challenges):
 print("#######################")
 
 
-start_date = datetime(2022, 9, 19).date()
-today_date = datetime.utcnow().date()
-challenge_day = (today_date - start_date).days
-today_challenge = challenges[challenge_day]
-
-
-print("Start date:",start_date)
-print("Today date:",today_date)
-print("Days between:",challenge_day)
-print("Challenge number:",challenge_day + 1)
-print("Total challenges:",len(challenges))
-print("Today's challenge:",today_challenge)
-print("#######################")
-
-
 vowels = "aaeeeiioou"
 consonants = "bbccddddffggghhjkllllmmnnnnnppqrrrrrrssssttttttvvwwxyyz"
 
+start_date = datetime(2022, 10, 1).date()
 
 def get_char(category):
     return random.choice(category)
@@ -60,16 +94,42 @@ def get_letters():
         category = random.choice([vowels,consonants])
         char = get_char(category)
         letters += char
-    
+
     shuffled = ''.join(random.sample(letters,len(letters)))
     #print(letters)
     #print(shuffled)
     return shuffled
 
 
+def get_matches(letters):
+    matches = []
+    for word in word_list:
+        check = 0
+        for char in word:
+            if word.count(char) > letters.upper().count(char):
+                check = 1
+        if check == 0:
+            matches.append(word)
+    return matches
+
+
+def get_count(matches):
+    count = {}
+    for x in range(16):
+        num = 0
+        for word in matches:
+            if len(word) == x + 1:
+                num += 1
+        if num > 0:
+            count["{}".format(x + 1)] = num
+    return count
+
+
+
 def check_word(answer):
     print("Checking dictionary...")
 
+    """
     app_id = '6ce7ceb9'
     app_key = 'bac47f553632715817395f753efc74d2'
     language = 'en-gb'
@@ -89,6 +149,11 @@ def check_word(answer):
 
     else:
         raise aiohttp.ClientError("Dictionary server failed to respond correctly")
+    """
+    if answer in word_list:
+        return len(answer)
+    else:
+        return 0
 
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -100,6 +165,7 @@ class Score(db.Model):
 
     def __repr__(self):
         score_dic = {
+            "id": self.id,
             "name": self.name,
             "score": self.score,
             "word": self.word,
@@ -116,15 +182,18 @@ def index():
 
 @app.route("/daily")
 def daily():
+    today_date = datetime.utcnow().date()
     return render_template("daily.html")
 
 
 @app.route("/free")
 def free():
-    return render_template("free.html")
+    #today_date = datetime.utcnow().date()
+    #return render_template("free.html")
+    return redirect("/daily")
 
 
-@app.route("/score", methods=["POST","GET"])
+@app.route("/scores", methods=["POST","GET"])
 def post_score():
     if request.method == "POST":
         if request.is_json:
@@ -167,7 +236,7 @@ def post_score():
 
     else:
         print("Score GET")
-        scores = Score.query.order_by(Score.score).filter(Score.challenge == (challenge_day + 1)).all()
+        scores = Score.query.order_by(Score.id).all()
         print(scores)
         print(type(scores))
 
@@ -175,7 +244,7 @@ def post_score():
         return res
 
 
-@app.route("/score/check", methods=["POST"])
+@app.route("/scores/check", methods=["POST"])
 def check_score():
     if request.is_json:
         req = request.get_json()
@@ -210,7 +279,7 @@ def check_score():
         return res
 
 
-@app.route("/score/<int:id>", methods=["GET","PUT","DELETE"])
+@app.route("/scores/<int:id>", methods=["GET","PUT","DELETE"])
 def delete(id):
     if request.method == "DELETE":
         score_delete = Score.query.get_or_404(id)
@@ -256,7 +325,7 @@ def delete(id):
 
                 if "challenge" in req:
                     score_update.challenge = req["challenge"]
-                
+
                 print(score_update)
                 db.session.commit()
 
@@ -290,35 +359,46 @@ def delete(id):
         return res
 
 
-@app.route("/board", methods=["GET","POST"])
+@app.route("/boards", methods=["GET","POST"])
 def board():
     if request.method == "POST":
         answer = request.form["word"]
         score = request.form["score"]
+        chall = request.form["challenge"]
 
         if int(score) > 0:
             message = "You scored " + score + " with the word " + answer + "!"
 
         else:
-            message = "Sorry, " + answer + " wasn't in the Oxford Dictionary. You scored 0."
+            message = "Sorry, " + answer + " wasn't in the word list. You scored 0."
 
-        return render_template("daily-board.html", message=message)
+        return render_template("daily-board.html", message=message, chall=chall)
 
     else:
         return render_template("board.html")
 
 
-@app.route("/challenge")
+@app.route("/challenges")
 def challenge():
+    today_date = datetime.utcnow().date()
+    time = datetime.utcnow()
+    challenge_day = (today_date - start_date).days
+    today_challenge = challenges[challenge_day]
+
     response = {
+        "start": start_date,
+        "today": today_date,
+        "time": time,
+        "daysbetween": challenge_day,
         "challenge": today_challenge,
-        "number": challenge_day + 1
+        "number": challenge_day + 1,
+        "total": len(challenges)
     }
     res = make_response(jsonify(response), 200)
     return res
-    
 
-@app.route("/challenge/<int:id>")
+
+@app.route("/challenges/<int:id>")
 def challenge_id(id):
     response = {
         "challenge": challenges[id - 1],
@@ -328,16 +408,33 @@ def challenge_id(id):
     return res
 
 
-@app.route("/random")
-def rand():
+@app.route("/letters")
+def rand_letters():
     letters = get_letters()
-
+    matches = get_matches(letters)
+    count = get_count(matches)
     response = {
-        "letters": letters
+        "letters": letters,
+        "matches": sorted(matches, key=len, reverse=True),
+        "count" : count
     }
     res = make_response(jsonify(response), 200)
     return res
-    
+
+
+@app.route("/letters/<let>")
+def check_letters(let):
+    letters = let
+    matches = get_matches(letters)
+    count = get_count(matches)
+    response = {
+        "letters": letters,
+        "matches": sorted(matches, key=len, reverse=True),
+        "count" : count
+    }
+    res = make_response(jsonify(response), 200)
+    return res
+
 
 if __name__ == "__main__":
     app.run(debug=True)
